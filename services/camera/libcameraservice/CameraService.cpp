01ce7004076a3b13e84f46e0e8c01c28a488538a
Darkobas <darkobas@gmail.com>
2020-05-20 10:09:48 +0200

    cameraservice: move client_package_name to data/misc
    Change-Id: I0d244b81625fd9514000bea313a28ad60321333f

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -2528,7 +2528,7 @@ status_t CameraService::BasicClient::startCameraOps() {
     sCameraService->mUidPolicy->registerMonitorUid(mClientUid);
 
 #ifdef TARGET_NEEDS_CLIENT_INFO
-    std::ofstream cpf("/data/vendor/zenx/client_package_name");
+    std::ofstream cpf("/data/misc/zenx/client_package_name");
     std::string cpn = String8(mClientPackageName).string();
     cpf << cpn;
 #endif
